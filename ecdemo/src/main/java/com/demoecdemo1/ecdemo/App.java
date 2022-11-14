b8a0c6d935049cb218adeb9e9a6a0ced6f67fc6f
ravijainemlink <rjain@emlinktechnologies.com>
2022-11-14 19:19:33 +0530

    New Update

--- ecdemo/src/main/java/com/demoecdemo1/ecdemo/App.java
+++ ecdemo/src/main/java/com/demoecdemo1/ecdemo/App.java
@@ -9,6 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! updated  " );
-        System.out.println( "Change on github  " );
+        System.out.println( "Change on github new " );
+        
+        for (int i=1;i<10 ;i++ )
+		{
+					System.out.println("Hello World...."+i);
+		}
     }
 }
